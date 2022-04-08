4e3d1b53cdcd3be3c7e4dde2de008b88e272ae29
Nadav Golbandi <nadavg@google.com>
2022-04-08 16:18:42 +0300

    js

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
 let notes = [
-    {
-        id: new Date(),
-        title: 'Sample Note',
-        body: 'This is a descreptive for our sample note',
-       bgColor: 'pink'
-    }
-]
+  {
+    id: new Date(),
+    title: "Sample Note",
+    body: "This is a descreptive for our sample note",
+    bgColor: "pink",
+  },
+];
 
-const createElement= (tag, classes = []) => {
+const createElement = (tag, classes = []) => {
   const element = document.createElement(tag);
-  classes.forEach(cl => {
-      element.classList.add(cl);
-  })
+  classes.forEach((cl) => {
+    element.classList.add(cl);
+  });
   return element;
-}
+};
 
 const createNoteView = (note) => {
-    const noteDiv = createElement('div', ['note']);
-    noteDiv.id = note.id;
-    const textDiv = createElement('div', ['text']);
-    textDiv.style.background = note.bgColor;
-    const titleP = createElement('b', ['title']);
-    titleP.innerHTML = note.title;
-    const bodyP = createElement('p', ['body']);
-    bodyP.innerHTML = note.body;
-    const editButton = createElement('button', ['edit']);
-    editButton.innerHTML = 'Edit Note';
-    const deleteButton = createElement('button', ['delete']);
-    deleteButton.innerHTML = 'Delete Note';
+  const noteDiv = createElement("div", ["note"]);
+  noteDiv.id = note.id;
+  const textDiv = createElement("div", ["text"]);
+  textDiv.style.background = note.bgColor;
+  const titleP = createElement("b", ["title"]);
+  titleP.innerHTML = note.title;
+  const bodyP = createElement("p", ["body"]);
+  bodyP.innerHTML = note.body;
+  const editButton = createElement("button", ["edit"]);
+  editButton.innerHTML = "Edit Note";
+  const deleteButton = createElement("button", ["delete"]);
+  deleteButton.innerHTML = "Delete Note";
 
-    textDiv.append(titleP);
-    textDiv.append(bodyP);
-    noteDiv.append(textDiv);
-    noteDiv.append(editButton);
-    noteDiv.append(deleteButton);
-    return noteDiv;
-}
+  textDiv.append(titleP);
+  textDiv.append(bodyP);
+  noteDiv.append(textDiv);
+  noteDiv.append(editButton);
+  noteDiv.append(deleteButton);
+  return noteDiv;
+};
 
-const notesDiv = document.querySelector('.notesDiv');
+const notesDiv = document.querySelector(".notesDiv");
 
 notes.forEach(note => {
-    const noteDiv = createNoteView(note);
-    notesDiv.append(noteDiv);
+  const noteDiv = createNoteView(note);
+  notesDiv.append(noteDiv);
 })
